3f1082e71c75cdfbaf24f819da6c3b7d8f655fb6
BPitts8019 <bradleyj.pitts@gmail.com>
2020-01-09 19:30:07 -0700

    Added some recipes

--- data/seeds/01-recipes.js
+++ data/seeds/01-recipes.js
@@ -1,13 +1,8 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+exports.seed = async (knex) => {
+   await knex("recipes").insert([
+      {name: "Noodle Surprise"},
+      {name: "Spaghetti Sauce"},
+      {name: "Meat Loaf"},
+      {name: "Apple Pie"}
+   ]);
+}
