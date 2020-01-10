5c2d03df4fe3f90a8ea291f7348186904dd139d2
BPitts8019 <bradleyj.pitts@gmail.com>
2020-01-09 19:04:45 -0700

    Setup Knex config and a first attempt at migration definition

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
       client: 'sqlite3',
       useNullAsDefault: true,
       connection: {
-         filename: './data/[something].db3'
+         filename: './data/recipe-book.db3'
       },
       migrations: {
          directory: "./data/migrations"
