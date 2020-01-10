80cc33fac1b53b4e1445d5e2c5d24a3fd44ddb23
BPitts8019 <bradleyj.pitts@gmail.com>
2020-01-09 19:27:52 -0700

    Added some ingredients

--- data/seeds/02-ingredients.js
+++ data/seeds/02-ingredients.js
@@ -1,13 +1,17 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+exports.seed = async (knex) => {
+   await knex("ingredients").insert([
+      {name: "Chicken Broth"},
+      {name: "Tomato Paste"},
+      {name: "Flour"},
+      {name: "Egg"},
+      {name: "Garlic"},
+      {name: "Ground Beef"},
+      {name: "Onion"},
+      {name: "Celery"},
+      {name: "Mushrooms"},
+      {name: "Spaghetti"},
+      {name: "Avocado"},
+      {name: "Vegetable Oil"},
+      {name: "Olive Oil"}
+   ]);
+}
