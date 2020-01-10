587ffc4d0a2f34b1d74c021ae9a70c02e0d6e55f
BPitts8019 <bradleyj.pitts@gmail.com>
2020-01-10 15:50:13 -0700

    Updated recipes-router to export an empty route

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const server = require("./server");
 const port = 5000;
 
 server.listen(port, () => {
-   console.log(`Server listening on http://localhost:${port}/...`);
+   console.log(`Server listening on http://localhost:${port}/ ...`);
 });
